6bea33ca68cff0ca584e5f938ee03ac20c770209
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 22:58:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,7 +5,7 @@ end
 def sort_array_desc(array)
   array.sort do |left, right|
     right <=> left
-end
+  end
 end
 
 def sort_array_char_count(array)
@@ -13,3 +13,19 @@ def sort_array_char_count(array)
     left.length <=> right.length
   end
 end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+

7a5354c5ca896005a99e3377a4375fc8bd8e2c5f
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 22:55:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,13 @@
 def sort_array_asc(array)
   array.sort
 end
-# sort_array_asc([25, 7, 1])
 
 def sort_array_desc(array)
   array.sort do |left, right|
     right <=> left
 end
 end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length

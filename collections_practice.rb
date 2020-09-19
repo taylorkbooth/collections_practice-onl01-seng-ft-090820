2d96dd192db671621a0ce770ef6f9148739e14fe
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 23:01:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,12 +42,12 @@ def find_a(array)
     sum
   end
   
-  def add_s(array)
-    array.collect do |word|
-      if array[1] == word
-        word
-      else
-        word + "s"
-      end
-    end
-  end
+  # def add_s(array)
+  #   array.collect do |word|
+  #     if array[1] == word
+  #       word
+  #     else
+  #       word + "s"
+  #     end
+  #   end
+  # end
